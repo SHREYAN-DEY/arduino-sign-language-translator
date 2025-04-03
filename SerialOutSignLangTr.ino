0db35f308e141d822fc09f1f29aea060b9033386
Sdey <92775779+SHREYAN-DEY@users.noreply.github.com>
2025-04-03 07:16:10 -0700

    SerialOutSignLangTr.ino

--- SerialOutSignLangTr.ino
+++ SerialOutSignLangTr.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include <MPU6050.h>
 
@@ -21,57 +20,40 @@ void setup() {
   Serial.begin(9600);
 
   // Initialize MPU6050
-  Serial.println("Initialize MPU6050");
-  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
-  }
-  Serial.println("MPU6050 Initialized");
-  checkSettings();
-}
-
-void checkSettings() {
-  Serial.println();
-  Serial.print(" * Sleep Mode:            ");
-  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
-
-  Serial.print(" * Clock Source:          ");
-  switch (mpu.getClockSource()) {
-    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
-    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
-    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
-    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
-    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
-    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  Serial.println("Initializing MPU6050...");
+  mpu.initialize();
+  
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 connection failed! Check wiring.");
+    while (1);
   }
 
-  Serial.print(" * Accelerometer offsets: ");
-  Serial.print(mpu.getAccelOffsetX());
-  Serial.print(" / ");
-  Serial.print(mpu.getAccelOffsetY());
-  Serial.print(" / ");
-  Serial.println(mpu.getAccelOffsetZ());
-  Serial.println();
+  Serial.println("MPU6050 Connected.");
 }
 
 void loop() {
-  // Read flex sensors
+  // Read flex sensor values
   flex1 = analogRead(adc1);
   flex2 = analogRead(adc2);
   flex3 = analogRead(adc3);
   flex4 = analogRead(adc4);
 
-  // Get normalized accelerometer values
-  Vector normAccel = mpu.readNormalizeAccel();
+  // Get raw accelerometer data
+  int16_t ax, ay, az;
+  mpu.getAcceleration(&ax, &ay, &az);
 
-  // Print normalized accelerometer data
+  // Convert to G-forces (assuming range ±2G, scale factor = 16384 LSB/G)
+  float normAx = ax / 16384.0;
+  float normAy = ay / 16384.0;
+  float normAz = az / 16384.0;
+
+  // Print accelerometer values
   Serial.print("Xnorm = ");
-  Serial.print(normAccel.XAxis);
+  Serial.print(normAx);
   Serial.print(" Ynorm = ");
-  Serial.print(normAccel.YAxis);
+  Serial.print(normAy);
   Serial.print(" Znorm = ");
-  Serial.println(normAccel.ZAxis);
+  Serial.println(normAz);
 
   // Print flex sensor values
   Serial.print(flex1);
@@ -90,51 +72,51 @@ void loop() {
     // Logic to control LED and print messages based on sensor values
     if ((flex2 < 200) && (flex1 < 200)) {
       digitalWrite(ledd, LOW);
-      if (normAccel.XAxis > 5) {
+      if (normAx > 0.3) {
         Serial.println("YES");
-      } else if (normAccel.XAxis < -3) {
+      } else if (normAx < -0.2) {
         Serial.println("NO");
-      } else if (normAccel.YAxis > 3) {
+      } else if (normAy > 0.2) {
         Serial.println("OK");
-      } else if (normAccel.YAxis < -3) {
+      } else if (normAy < -0.2) {
         Serial.println("BYE");
       }
     }
     else if (flex1 < 200) {
       digitalWrite(ledd, LOW);
-      if (normAccel.XAxis > 5) {
+      if (normAx > 0.3) {
         Serial.println("THANK YOU");
-      } else if (normAccel.XAxis < -5) {
+      } else if (normAx < -0.3) {
         Serial.println("NICE TO MEET YOU");
-      } else if (normAccel.YAxis > 5) {
+      } else if (normAy > 0.3) {
         Serial.println("Glad to be here");
-      } else if (normAccel.YAxis < -5) {
+      } else if (normAy < -0.3) {
         Serial.println("HELLO, this is my project");
         delay(1500);  // Allow time for message to be visible
       }
     }
     else if (flex2 < 200) {
       digitalWrite(ledd, LOW);
-      if (normAccel.XAxis > 5) {
+      if (normAx > 0.3) {
         Serial.println("WATER");
-      } else if (normAccel.XAxis < -5) {
+      } else if (normAx < -0.3) {
         Serial.println("WHAT IS YOUR NAME");
         delay(500);  // Allow time for message to be visible
-      } else if (normAccel.YAxis > 5) {
+      } else if (normAy > 0.3) {
         Serial.println("SORRY");
-      } else if (normAccel.YAxis < -5) {
+      } else if (normAy < -0.3) {
         Serial.println("Fine");
       }
     }
     else {
       digitalWrite(ledd, HIGH);
-      if (normAccel.XAxis > 5) {
+      if (normAx > 0.3) {
         Serial.println("HELP");
-      } else if (normAccel.XAxis < -5) {
+      } else if (normAx < -0.3) {
         Serial.println("WASHROOM");
-      } else if (normAccel.YAxis > 5) {
+      } else if (normAy > 0.3) {
         Serial.println("Medicine");
-      } else if (normAccel.YAxis < -5) {
+      } else if (normAy < -0.3) {
         Serial.println("FOOD");
       }
     }
